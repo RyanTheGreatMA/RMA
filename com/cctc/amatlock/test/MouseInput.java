63aef4b2c0847180f1226c605fe770f85f9b0a53
RyanTheGreatMA <49995332+RyanTheGreatMA@users.noreply.github.com>
2019-04-30 13:17:31 -0500

    Add files via upload

---
+++ com/cctc/amatlock/test/MouseInput.java
@@ -0,0 +1,18 @@
+package com.cctc.amatlock.test;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MouseInput implements MouseMotionListener {
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        mouseMoved();
+    }
+}
