370d1cec4f3887d9f1ebcdb21a4c53457fdb4623
RyanTheGreatMA <49995332+RyanTheGreatMA@users.noreply.github.com>
2019-05-10 14:15:27 -0500

    Add files via upload

--- com/cctc/amatlock/test/utilities/ResourceLoader.java
+++ com/cctc/amatlock/test/utilities/ResourceLoader.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 public class ResourceLoader
 {
+    private static ImageLoader imageLoader = new ImageLoader();
+    private static SoundLoader soundLoader = new SoundLoader();
     // Creates an instance of ImageLoader
     // This is used to load each image
-    private static ImageLoader imageLoader = new ImageLoader();
+
 
     /**
      * This method will load and store every image
@@ -22,7 +24,20 @@ public class ResourceLoader
             Images.ship = imageLoader.loadImage("ship.png");
             Images.goei = imageLoader.loadImage("goei.png");
             Images.background = imageLoader.loadImage("background.png");
-        } catch (IOException e)
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void loadSounds()
+    {
+        // The try/catch is code to catch if
+        // any image isn't found.
+        try{
+//            Sounds.background = soundLoader.loadSound("background.wav");
+            Sounds.alien = soundLoader.loadSound("alien.wav");
+
+        } catch (Exception e)
         {
             e.printStackTrace();
         }
